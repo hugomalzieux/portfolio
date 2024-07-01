9c8d18f96e9a665ac77e62bbef0dd9dd1b2d7c55
hugomalzieux <hmalzieux@gmail.com>
2024-07-01 18:28:09 +1200

    feat(mailer): trying mailgun

--- routes/api/mail/index.ts
+++ routes/api/mail/index.ts
@@ -1,28 +1,22 @@
 import { Handlers } from "$fresh/server.ts";
 import "https://deno.land/x/dotenv@v3.2.2/load.ts";
-import formData from "npm:form-data";
-import Mailgun from "mailgun.js";
+import Mailgun from "https://deno.land/x/mailgun@v1.1.0/index.ts";
+
 const { SMTP_USER, MAILGUN_API_KEY, MAILGUN_DOMAIN } = Deno.env.toObject();
 
 export const handler: Handlers<string | null> = {
   async POST(req, _ctx) {
-    const mailgun = new Mailgun(formData);
+    const mailgun = new Mailgun({
+      key: MAILGUN_API_KEY,
+      domain: MAILGUN_DOMAIN,
+    });
 
-    const client = mailgun.client({ username: "api", key: MAILGUN_API_KEY });
     const { sender, message } = await req.json();
-
-    const messageData = {
+    return await mailgun.send({
       from: sender, // sender address
       to: SMTP_USER, // list of receivers
       subject: `[Portfolio] - New message from ${sender}`,
       text: message,
-    };
-
-    client.messages
-      .create(MAILGUN_DOMAIN, messageData)
-      .then((res) => console.log(res))
-      .catch((err) => console.error(err));
-
-    return new Response("Email sent!");
+    });
   },
 };
